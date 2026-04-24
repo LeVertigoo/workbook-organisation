b58078f032472e8796bc3cc55dd1eb1e0d239968
Thomas <130591884+LeVertigoo@users.noreply.github.com>
2026-04-24 12:09:14 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorkbookOrga from './WorkbookOrga.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorkbookOrga />
+  </React.StrictMode>
+)
